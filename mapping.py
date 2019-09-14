a1535c91ca878928dc49217696409798913cbd45
Syed Safwaan <syed.safwaan@gmail.com>
2019-09-14 02:28:44 -0400

    Added `mapping.py`

---
+++ mapping.py
@@ -0,0 +1,59 @@
+from mapping_util import *
+from pygame import *
+
+init()
+
+map_img = image.load('notcoding.png')
+screen_size = get_start_dims(map_img)
+
+screen = display.set_mode(screen_size, RESIZABLE)
+display.set_caption('geiii')
+
+clock = time.Clock()
+
+screen_zoom = get_zoom(map_img, *screen_size)
+manual_zoom = 1
+angle = 0
+pos = 0, 0
+
+new_map_img = transform.rotozoom(map_img, angle, screen_zoom * manual_zoom)
+
+click_and_drag = False
+
+points = set()
+
+while True:
+    e = event.wait()
+    if e.type == QUIT:
+        break
+    elif e.type == MOUSEBUTTONDOWN:
+        pass
+    elif e.type == MOUSEBUTTONUP:
+        if e.button == 4:
+            manual_zoom += 0.05
+            new_map_img = transform.rotozoom(map_img, angle, screen_zoom * manual_zoom)
+        elif e.button == 5:
+            manual_zoom -= 0.05
+            new_map_img = transform.rotozoom(map_img, angle, screen_zoom * manual_zoom)
+        else:
+            mx, my = e.pos
+            mx = (mx - pos[0]) / ((screen_zoom * manual_zoom) * map_img.get_width())
+            my = (my - pos[1]) / ((screen_zoom * manual_zoom) * map_img.get_height())
+            points.add((mx, my))
+            print((mx, my))
+    elif e.type == MOUSEMOTION:
+        print('wow')
+    elif e.type == VIDEORESIZE:
+        screen_size = e.size
+        screen_zoom = get_zoom(map_img, *screen_size)
+        new_map_img = transform.rotozoom(map_img, angle, screen_zoom * manual_zoom)
+        display.set_mode(screen_size, RESIZABLE)
+
+    screen.fill((0, 0, 0))
+    screen.blit(new_map_img, pos)
+
+    # clock.tick()
+    # display.set_caption(f'2cool {clock.get_fps()}')
+    display.update()
+
+display.quit()
---
+++ mapping_util.py
@@ -0,0 +1,23 @@
+from pygame import *
+
+
+def get_zoom(img, new_width=1280, new_height=720):
+    screen_ratio = new_width / new_height
+    img_ratio = img.get_width() / img.get_height()
+
+    if screen_ratio > img_ratio:  # if screen is a "wider" image than supplied image
+        return new_height / img.get_height()
+    else:  # if screen is less wide than the image
+        return new_width / img.get_width()
+
+
+def get_start_dims(img, new_width=1280, new_height=720):
+    specs = display.Info()
+
+    screen_ratio = specs.current_w / specs.current_h
+    img_ratio = img.get_width() / img.get_height()
+
+    if screen_ratio > img_ratio:  # if screen is a "wider" image than supplied image
+        return round(img.get_width() * new_height / img.get_height()), new_height
+    else:  # if screen is less wide than the image
+        return new_width, round(img.get_height() * new_width / img.get_width())
